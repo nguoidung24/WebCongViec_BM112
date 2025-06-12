f77819d673c3eb19e992acc7696f6eb73a2eb9b8
nguoidung24 <dungtamtithoi88@gmail.com>
2025-06-12 10:34:02 +0700

    Xong thêm phát sinh, bắt đầu hiển thị danh sách phát sinh

---
+++ WebCongViec_v2/Controllers/Admin/PhatSinhController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebCongViec_v2.Models;
+using WebCongViec_v2.Services;
+
+namespace WebCongViec_v2.Controllers.Admin
+{
+    public class PhatSinhController : Controller
+    {
+        protected PhatSinhService phatSinhService;
+        public PhatSinhController()
+        {
+            this.phatSinhService = new PhatSinhService();
+        }
+
+        [Route("PhatSinh")]
+        public IActionResult PhatSinh(string action,List<int> IdNhanSu, Phatsinh phatSinh)
+        {
+            ViewBag.BangLuong = "active";
+            ViewBag.DSNhanSu = this.phatSinhService.dsNhanSu();
+            if(action != null && action.Equals("create"))
+            {
+                if (IdNhanSu != null && IdNhanSu.Count > 0 && phatSinh != null)
+                {
+                    if (this.phatSinhService.themPhatSinh(IdNhanSu, phatSinh))
+                    {
+                        return RedirectToAction("PhatSinh", "PhatSinh", new { message = "Thành công!", messageType = "success" });
+
+                    }
+                    else
+                    {
+                        return RedirectToAction("PhatSinh", "PhatSinh", new { message = "Không thành công!", messageType = "warning" });
+
+                    }
+                }
+                else
+                {
+                    return RedirectToAction("PhatSinh", "PhatSinh", new { message = "Không thành công!", messageType = "warning" });
+
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ WebCongViec_v2/Services/PhatSinhService.cs
@@ -0,0 +1,34 @@
+﻿using WebCongViec_v2.Models;
+
+namespace WebCongViec_v2.Services
+{
+    public class PhatSinhService : BaseService
+    {
+        public Object dsNhanSu()
+        {
+            var dsNhanSu = this.DbContext.Nhansus
+                .Where(n => n.Status == 1)
+                .Select(n => new { n.IdNhanSu, n.HoTenNhanSu })
+                .ToList();
+
+            return dsNhanSu;
+        }
+
+        public bool themPhatSinh(List<int> IdNhanSu, Phatsinh phatSinh)
+        {
+            foreach (var id in IdNhanSu)
+            {
+                this.DbContext.Phatsinhs.Add(new Phatsinh
+                {
+                    IdNhanSu = id,
+                    NoiDungPhatSinh = phatSinh.NoiDungPhatSinh,
+                    GiaTriPhatSinh = phatSinh.GiaTriPhatSinh,
+                    LoaiPhatSinh = phatSinh.LoaiPhatSinh,
+                    NgayTinhPhatSinh = phatSinh.NgayTinhPhatSinh
+                }); 
+            }
+            this.DbContext.SaveChanges();
+            return true;
+        }
+    }
+}
