40d4eb091c13efdd4c966bc250e51f045f1a2ee3
nguoidung24 <dungtamtithoi88@gmail.com>
2025-06-13 10:04:52 +0700

    Sửa giao diện

---
+++ WebCongViec_v2/Controllers/Admin/NangSuatController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebCongViec_v2.Controllers.Admin
+{
+    public class NangSuatController : Controller
+    {
+        [Route("NangSuat")]
+        public IActionResult NangSuat()
+        {
+            return View();
+        }
+    }
+}
