b331be77ea37637478a355398c9d846a005e21d9
nguoidung24 <dungtamtithoi88@gmail.com>
2025-07-23 17:34:22 +0700

    Sửa hiển thị năng xuất dạng float. start CapNhatNS

---
+++ WebCongViec_v2/Controllers/Admin/CapNhatNSController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace WebCongViec_v2.Controllers.Admin
+{
+    public class CapNhatNSController : Controller
+    {
+        [Route("/CapNhatNS")]
+        public IActionResult CapNhatNS()
+        {
+            return View();
+        }
+
+        /*Sử dụng using ClosedXML.Excel;*/
+        [HttpPost]
+        [Route("/CapNhatNS")]
+        public IActionResult CapNhatNS(IFormFile fileData)
+        {
+            if (fileData != null && fileData.Length > 0)
+            {
+
+                ViewBag.FileName = $"Chọn file: {fileData.FileName}";
+            }
+            else
+            {
+                ViewBag.FileName = "Không nhận được file nào.";
+            }
+
+            return View();
+        }
+
+    }
+}
