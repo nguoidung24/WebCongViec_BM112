fb915521edb176eb8d06580f9b16794a18c9f098
nguoidung24 <dungtamtithoi88@gmail.com>
2025-07-29 10:25:50 +0700

    End Game

--- WebCongViec_v2/Controllers/Admin/CapNhatNSController.cs
+++ WebCongViec_v2/Controllers/Admin/CapNhatNSController.cs
@@ -10,10 +10,12 @@ namespace WebCongViec_v2.Controllers.Admin
     public class CapNhatNSController : Controller 
     {
         protected CapNhatNSService capNhatNSService;
+        private readonly IWebHostEnvironment _env;
 
-        public CapNhatNSController()
+        public CapNhatNSController(IWebHostEnvironment env)
         {
             this.capNhatNSService = new CapNhatNSService();
+            _env = env;
 
         }
 
@@ -35,6 +37,23 @@ namespace WebCongViec_v2.Controllers.Admin
             (ViewBag.FileName, ViewBag.SuccessMessage, ViewBag.ErrorMessage, ViewBag.ExcelData) = this.capNhatNSService.getExcelData(fileData);  
             ViewBag.NhanSuList = this.capNhatNSService.getAllNhanSu();
             return View();
+        }
+
+        public IActionResult DownloadNSTempExcel()
+        {
+            string filePath = Path.Combine(_env.WebRootPath, WebCongViec_v2.Services.ProjectService.GetProjectName().Replace("/", ""), "files/temp_cc.xlsx");
+
+            if (!System.IO.File.Exists(filePath))
+            {
+                return NotFound("File không tồn tại.");
+            }
+
+            byte[] fileBytes = System.IO.File.ReadAllBytes(filePath);
+            string fileName = "temp_cc.xlsx";
+
+            return File(fileBytes,
+                        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                        fileName);
         }
     }
 }
--- WebCongViec_v2/Controllers/LoginController.cs
+++ WebCongViec_v2/Controllers/LoginController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿/*using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.NetworkInformation;
 using WebCongViec_v2.Middleware;
@@ -14,7 +14,7 @@ namespace WebCongViec_v2.Controllers
             this._context = new Bm112Context();
         }
 
-/*        [Microsoft.AspNetCore.Mvc.Route("/login")]
+*//*        [Microsoft.AspNetCore.Mvc.Route("/login")]
         public IActionResult Index()
         {
             ViewBag.AppName = "CN - 13";
@@ -24,7 +24,7 @@ namespace WebCongViec_v2.Controllers
                 ViewBag.AppName = AppName.Data;  
             }
             return View();
-        }*/
+        }*//*
 
         string? bm12Decode(string? input)
         {
@@ -37,6 +37,103 @@ namespace WebCongViec_v2.Controllers
         {
             string? id = bm12Decode(username);
             string password = "12345678";
+            *//* string mac = "";
+             NetworkInterface[] networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+             foreach (NetworkInterface netInterface in networkInterfaces)
+             {
+                 if (netInterface.OperationalStatus == OperationalStatus.Up && netInterface.NetworkInterfaceType != NetworkInterfaceType.Loopback && netInterface.NetworkInterfaceType != NetworkInterfaceType.Tunnel)
+                 {
+                     PhysicalAddress macAddress = netInterface.GetPhysicalAddress();
+                     mac = macAddress.ToString();
+                     break;
+                 }
+             }
+             if (!mac.Equals("00090FAA0001"))
+             {
+                 return View();
+             }*//*
+
+            if (id != null && password != null)
+            {
+                if (!id.Equals("") && !password.Equals(""))
+                {
+                    string filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "admin.txt");
+
+                    if (System.IO.File.Exists(filePath))
+                    {
+                        string fileContent = System.IO.File.ReadAllText(filePath);
+                        if (id.Equals(fileContent))
+                        {
+                            var isAdmin = _context.DbWebChamCongV3s.Where(row => row.Id.Equals("0")).First();
+                            if(isAdmin != null)
+                                if (password.Equals(isAdmin.Data))
+                                {
+                                    HttpContext.Session.SetString("isLogin", "1");
+                                    HttpContext.Session.SetString("DisplayName", "Admin");
+                                    HttpContext.Session.SetString("role", "1");
+                                    HttpContext.Session.SetString("ID", "Admin");
+                                    return RedirectToAction("Index", "/");
+                                }
+                        }
+                    }
+
+                    Nhansu? check = _context.Nhansus.Where(row => row.IdNhanSu.ToString().Equals(id) && row.Password.Equals(password)).FirstOrDefault();
+                    if (check != null)
+                    {
+                        HttpContext.Session.SetString("isLogin", "1");
+                        HttpContext.Session.SetString("DisplayName", check.HoTenNhanSu);
+                        HttpContext.Session.SetString("ID", check.IdNhanSu.ToString());
+                        HttpContext.Session.SetString("userrole", check.Role.ToString());
+                        HttpContext.Session.SetString("HasInfo", check.Status.ToString());
+                        HttpContext.Session.SetString("role", "0");
+                        return RedirectToAction("Index", "HomeUsers");
+                    }
+                }
+            }
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
+*/
+
+
+
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.NetworkInformation;
+using WebCongViec_v2.Middleware;
+using WebCongViec_v2.Models;
+
+namespace WebCongViec_v2.Controllers
+{
+    public class LoginController : Controller
+    {
+        protected Bm112Context _context;
+        public LoginController()
+        {
+            this._context = new Bm112Context();
+        }
+
+        [Microsoft.AspNetCore.Mvc.Route("/login")]
+        public IActionResult Index()
+        {
+            ViewBag.AppName = "CN - 13";
+            var AppName = _context.DbWebChamCongV3s.Where(row => row.Id.Equals("appname")).FirstOrDefault();
+            if (AppName != null)
+            {
+                ViewBag.AppName = AppName.Data;
+            }
+            return View();
+        }
+
+
+
+        [Microsoft.AspNetCore.Mvc.Route("/login")]
+        [HttpPost]
+        public IActionResult Index(string id, string password)
+        {
             /* string mac = "";
              NetworkInterface[] networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
 
@@ -66,7 +163,7 @@ namespace WebCongViec_v2.Controllers
                         if (id.Equals(fileContent))
                         {
                             var isAdmin = _context.DbWebChamCongV3s.Where(row => row.Id.Equals("0")).First();
-                            if(isAdmin != null)
+                            if (isAdmin != null)
                                 if (password.Equals(isAdmin.Data))
                                 {
                                     HttpContext.Session.SetString("isLogin", "1");
